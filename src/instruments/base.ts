fce4669285a7177205fac1db51d5da51b8c096e0
Joel <jlouzado@gmail.com>
2020-09-13 21:22:40 +0530

    refactor(base): remove constructor

--- src/instruments/base.ts
+++ src/instruments/base.ts
@@ -4,12 +4,6 @@ export type Options = {
 }
 
 export abstract class Instrument {
-  ctx: CanvasRenderingContext2D | null
-
-  constructor(ctx: CanvasRenderingContext2D) {
-    this.ctx = ctx
-  }
-
   abstract drawStart(e: MouseEvent, options: Options): void
   abstract drawEnd(e: MouseEvent): void
   abstract draw(e: MouseEvent, options: Options): void
