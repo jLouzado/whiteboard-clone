060fe0d3e7be10c540351f494c638ebb1c781819
Joel <jlouzado@gmail.com>
2020-09-13 12:28:15 +0530

    feat(canvas): draw on UI

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 const app = document.getElementById('root')
 const canvas = document.getElementById('canvas') as HTMLCanvasElement | null
 
-let context: CanvasRenderingContext2D | null = null
+let ctx: CanvasRenderingContext2D | null = null
 
 const height = 300
 const width = 300
 
 const state = {
-  drawing: false
+  drawing: false,
+  width: 5, // pixels
+  color: 'blue'
 }
 
 const resizeCanvas = (canvas: HTMLCanvasElement, window: Window) => {
@@ -15,15 +17,39 @@ const resizeCanvas = (canvas: HTMLCanvasElement, window: Window) => {
   canvas.width = window.innerWidth
 }
 
-const drawSquare = (context: CanvasRenderingContext2D) => {
-  context.fillStyle = 'red'
-  context.fillRect(10, 10, 50, 50)
+const drawStart = (context: CanvasRenderingContext2D) => (e: MouseEvent) => {
+  console.log('starting')
+  state.drawing = true
+  context.beginPath()
+  drawing(context)(e)
+}
+
+const drawEnd = (context: CanvasRenderingContext2D) => () => {
+  console.log('ending')
+  state.drawing = false
+  context.closePath()
+}
+
+const drawing = (context: CanvasRenderingContext2D) => (e: MouseEvent) => {
+  if (!state.drawing) return
+
+  context.lineCap = 'round'
+  context.lineWidth = state.width
+  context.strokeStyle = state.color
+
+  console.log('drawing')
+  context.lineTo(e.clientX, e.clientY)
+  context.stroke()
 }
 
 window.onload = () => {
   if (canvas) {
     resizeCanvas(canvas, window)
-    context = canvas?.getContext('2d') ?? null
-    if (context) drawSquare(context)
+    ctx = canvas?.getContext('2d') ?? null
+    if (ctx) {
+      window.addEventListener('mousedown', drawStart(ctx))
+      window.addEventListener('mouseup', drawEnd(ctx))
+      window.addEventListener('mousemove', drawing(ctx))
+    }
   }
 }
