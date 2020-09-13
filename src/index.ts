66136bf3834b20c7ac3a9ce364fc7f32a690c4fe
Joel Louzado <jlouzado@gmail.com>
2020-09-13 05:13:08 +0000

    chore(config): configure prettier & tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-document.getElementById("app").innerHTML = `
+const app = document.getElementById('app')
+
+if (app) {
+    app.innerHTML = `
 <h1>Hello Parcel!</h1>
 <div>
   Look
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
   for more info about Parcel.
 </div>
-`;
+`
+}
