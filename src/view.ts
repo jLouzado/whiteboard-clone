7389717bcae0a7d7bda7f4a82cde0a24e7f84c92
Joel <jlouzado@gmail.com>
2020-09-14 02:22:44 +0530

    feat(view): add styles and tooltips

--- src/view.ts
+++ src/view.ts
@@ -1,5 +1,6 @@
 import {h} from 'snabbdom/build/package/h'
 import {VNode} from 'snabbdom/build/package/vnode'
+import {stylesheet} from 'typestyle'
 import {Dispatcher, WBState, WIDTH} from './app'
 import {changeColor, changeInstrument, changeSize} from './reducers'
 
@@ -9,10 +10,27 @@ const instruments: ['Pen', 'Highlight', 'Eraser'] = [
   'Eraser'
 ]
 
+const styles = stylesheet({
+  container: {
+    $nest: {
+      '& > :not(:first-child)': {
+        margin: '6px 0'
+      }
+    }
+  },
+  button: {
+    height: '45px',
+    width: '60px',
+    borderRadius: '8px',
+    border: 'none'
+  }
+})
+
 const hButton = (
   handleClick: EventListener,
   child: string | number | VNode,
-  key: string
+  key: string,
+  title?: string
 ) =>
   h(
     'button',
@@ -20,6 +38,12 @@ const hButton = (
       key,
       on: {
         click: handleClick
+      },
+      class: {
+        [styles.button]: true
+      },
+      props: {
+        title: title ?? ''
       }
     },
     [child]
@@ -29,16 +53,26 @@ export const view = (dispatch: Dispatcher<WBState>, state: WBState) => {
   const colors = state.instrument.getSupportedColors
 
   // TODO(refactor): assert on this id, since it's needed for layout
-  return h('div#tools', [
+  return h('div#tools', {class: {[styles.container]: true}}, [
     ...instruments.map((i) =>
-      hButton(dispatch(changeInstrument(i)), i, `ins-${i}`)
+      hButton(dispatch(changeInstrument(i)), i, `ins-${i}`, `Activate ${i}`)
     ),
     ...[WIDTH.SMALL, WIDTH.MEDIUM, WIDTH.LARGE].map((width) =>
-      hButton(dispatch(changeSize(width)), width, `size-${width}`)
+      hButton(
+        dispatch(changeSize(width)),
+        width,
+        `size-${width}`,
+        `Set Width to ${width}`
+      )
     ),
     ...(colors
       ? colors.map((color) =>
-          hButton(dispatch(changeColor(color)), color, `col-${color}`)
+          hButton(
+            dispatch(changeColor(color)),
+            color,
+            `col-${color}`,
+            'Set Colour'
+          )
         )
       : [''])
   ])
