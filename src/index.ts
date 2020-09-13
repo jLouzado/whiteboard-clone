5bc395276c79af649ae9fe3f7d3cdb463295f58c
Joel <jlouzado@gmail.com>
2020-09-13 11:38:46 +0530

    feat(app): setup canvas

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
-const app = document.getElementById('app')
+const app = document.getElementById('root')
+const canvas = document.getElementById('canvas') as HTMLCanvasElement | null
 
-if (app) {
-    app.innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`
+let context: CanvasRenderingContext2D | null = null
+
+const height = 300
+const width = 300
+
+const state = {
+  drawing: false
+}
+
+const resizeCanvas = (canvas: HTMLCanvasElement, window: Window) => {
+  canvas.height = window.innerHeight
+  canvas.width = window.innerWidth
+}
+
+const drawSquare = (context: CanvasRenderingContext2D) => {
+  context.fillStyle = 'red'
+  context.fillRect(10, 10, 50, 50)
+}
+
+window.onload = () => {
+  if (canvas) {
+    resizeCanvas(canvas, window)
+    context = canvas?.getContext('2d') ?? null
+    if (context) drawSquare(context)
+  }
 }
